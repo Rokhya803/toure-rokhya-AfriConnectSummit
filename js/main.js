e1ed4c653576fbe774c825e4e7de8c9db6c263a7
Rokhya <tourerokhya943@gmail.com>
2026-07-27 13:03:13 +0000

    Ajout du mode sombre bleu nuit et amélioration du thème

--- js/main.js
+++ js/main.js
@@ -4,3 +4,39 @@ const navLinks = document.querySelector(".linknav");
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+// Button Mode Jour / Nuit
+const toggleBtn = document.getElementById('dark-mode');
+
+if(toggleBtn){
+
+    if(localStorage.getItem('theme') === 'dark'){
+
+        document.body.classList.add('dark-mode');
+
+        toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+
+    }
+
+
+    toggleBtn.addEventListener('click',()=>{
+
+        document.body.classList.toggle('dark-mode');
+
+        if(document.body.classList.contains('dark-mode')){
+
+            localStorage.setItem('theme','dark');
+
+            toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+
+        }else{
+
+            localStorage.setItem('theme','light');
+
+            toggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+
+        }
+
+    });
+
+}
